3452b69c89d1926af8882a58e8dbe41304fc3cae
Sasha-Vlasova <avlasova@carrollu.edu>
2026-02-18 03:19:17 +0000

    Home and About working now!!!

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,18 @@
-import FirstStepInReact from "./FirstStepInReact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
 
 function App() {
-  return <FirstStepInReact />;
+  return (
+    <Router>
+      <Navbar />   {/* <-- this will show links on every page */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
